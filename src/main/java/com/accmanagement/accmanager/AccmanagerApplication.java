eeb0f1bee6923202d8dd7376c3900ea1194dc154
Matvei.Barbashov <matvei.barbashov@luminorgroup.com>
2024-02-18 12:22:10 +0200

    Init

---
+++ src/main/java/com/accmanagement/accmanager/AccmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.accmanagement.accmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accmanagement/accmanager/AccmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accmanagement.accmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
